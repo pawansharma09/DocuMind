3e5759761f986032b3518890c25616e20f14ebf8
Pawan Sharma <136601439+pawansharma09@users.noreply.github.com>
2025-10-21 13:14:05 +0530

    Update vector_store_handler.py

--- src/vector_store_handler.py
+++ src/vector_store_handler.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_text_chunks(text):
     """
